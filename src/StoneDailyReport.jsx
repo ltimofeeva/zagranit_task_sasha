f98d20d40c5505bc8959efe94c79b14d11ded461
ltimofeeva <43849844+ltimofeeva@users.noreply.github.com>
2025-11-30 12:29:58 +0500

    Update StoneDailyReport.jsx

--- src/StoneDailyReport.jsx
+++ src/StoneDailyReport.jsx
@@ -230,7 +230,7 @@ export default function StoneDailyReport() {
     const dateToSend = formatDateForBackend(reportDate);
 
     await fetch(
-      "https://lpaderina.store/webhook/plan_bot_sasha",
+      "https://lpaderina.store/webhook/70e744f0-35d8-4252-ba73-25db1d52dbf9",
       {
         method: "POST",
         headers: { "Content-Type": "application/json" },
